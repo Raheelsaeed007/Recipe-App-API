7e79448f3d0639cc0e595bc73622dccfd84b36b0
Raheelsaeed007 <113680419+Raheelsaeed007@users.noreply.github.com>
2025-05-31 18:38:28 +0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,26 +11,25 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-// Middlewares
+
 app.use(cors());
 app.use(express.json());
 
-// Routes
+
 app.use("/api/auth", authRoutes);
 app.use("/api/recipes", recipesRoutes);
 
-// Static frontend (for Render/Vercel)
+
 const __dirname = path.resolve();
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-  // Catch-all route for React Router
-  app.get("/*path", (req, res) => {
+  app.get("/{*splat}", (req, res) =>{
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
 }
 
-// Start server only if DB connects
+
 connectDB()
   .then(() => {
     app.listen(PORT, () => {
